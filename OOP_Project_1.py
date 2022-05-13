e134b81301427f8612dbf6d27c4670f0a4158212
alex4845 <104935063+alex4845@users.noreply.github.com>
2022-05-13 23:23:21 +0300

    Add files via upload

--- OOP_Project_1.py
+++ OOP_Project_1.py
@@ -1,4 +1,8 @@
+
+b = []
+import datetime
 class Cheke_list():
+    count = 1
     def __init__(self, number, type, date, date_res, name, status):
         self.number = number
         self.type = type
@@ -6,6 +10,33 @@ class Cheke_list():
         self.date_res = date_res
         self.name = name
         self.status = status
+        Cheke_list.count += 1
+
+    @staticmethod
+    def new_client():
+
+        name = input("Введите свое ФИО ")
+        type = input("Введите какую технику сдаете в ремонт (телефон, ноутбук, телевизор) ")
+        mark = input("Введите марку ")
+        if mark == "apple":
+            o_s = "IOS"
+        else:
+            o_s = "android"
+        description = input("Введите неисправность ")
+
+        date = datetime.date.today().strftime("%d.%m.%Y")
+        if type == "телефон":
+            chek1 = Phone(Cheke_list.count, type, date, "16.05.2022", name, "в процессе", mark, o_s, description)
+
+        if type == "ноутбук":
+            ear = input("Какой год вашего ноутбука ")
+            chek4 = Book(Cheke_list.count, type, date, "17.05.2022", name, "в процессе", mark, o_s, ear, description)
+
+        if type == "телевизор":
+            size = input("Какая диагональ вашего телевизора ")
+            chek3 = Monitor(Cheke_list.count, type, date, "18.05.2022", name, "в процессе", mark, size, description)
+
+
 
 class Phone(Cheke_list):
     def __init__(self, number, type, date, date_res, name, status, mark, o_s, description):
@@ -13,14 +44,22 @@ class Phone(Cheke_list):
         self.mark = mark
         self.o_s = o_s
         self.description = description
+        a = [self.number, self.name, self.type, self.date, self.date_res,
+             self.status, self.mark, self.o_s, self.description]
+        b.append(a)
 
 class Book(Cheke_list):
+
     def __init__(self, number, type, date, date_res, name, status, mark, o_s, ear, description):
         super().__init__(number, type, date, date_res, name, status)
         self.mark = mark
         self.o_s = o_s
         self.ear = ear
         self.description = description
+        a = [self.number, self.name, self.type, self.date, self.date_res,
+             self.status, self.mark, self.o_s, self.ear, self.description]
+        b.append(a)
+
 
 class Monitor(Cheke_list):
     def __init__(self, number, type, date, date_res, name, status, mark, size, description):
@@ -28,24 +67,39 @@ class Monitor(Cheke_list):
         self.mark = mark
         self.size = size
         self.description = description
+        a = [self.number, self.name, self.type, self.date, self.date_res,
+             self.status, self.mark, self.size, self.description]
+        b.append(a)
+#    def print_chek(self):
 
-number = 1
-name = input("Введите свое ФИО ")
-type = input("Введите какую технику сдаете в ремонт (телефон, ноутбук, телевизор) ")
 
-if type == "телефон":
-    mark = input("Введите марку телефона ")
-    if mark == "apple":
-        o_s = "IOS"
-    else:
-        o_s = "android"
-    description = input("Введите неисправность ")
+chek2 = Book(Cheke_list.count, "ноутбук", "11.05.2022", "16.05.2022", "Иван Иванович Макашов",
+             "в работе", "apple", "IOS", "2015", "не включается")
+chek5 = Monitor(Cheke_list.count, "телевизор", datetime.date.today().strftime("%d.%m.%Y"), "19.05.2022", "Козловский Данила",
+                "принят в работу", "toshiba", "105", "нет звука")
+chek6 = Phone(Cheke_list.count, "телефон", datetime.date.today().strftime("%d.%m.%Y"), "21.05.2022", "Козловский Данила",
+                "принят в работу", "nokia", "android", "побит экран")
 
-    chek1 = Phone(number, type, "11.05.2022", "16.05.2022", name, "готов", mark,
-                  o_s, description)
-    a = [chek1.number, chek1.name, chek1.type, chek1.date, chek1.date_res,
-         chek1.status, chek1.mark, chek1.o_s, chek1.description]
-print(a)
+Cheke_list.new_client()
+
+
+
+print("Всего квитанций", Cheke_list.count-1)
+for i in b:
+    print (i)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
