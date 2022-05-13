2faa5e18b19aa865edc6898e912851ee25387e2c
alex4845 <104935063+alex4845@users.noreply.github.com>
2022-05-14 00:52:10 +0300

    Add files via upload

--- OOP_Project_1.py
+++ OOP_Project_1.py
@@ -13,6 +13,20 @@ class Cheke_list():
         Cheke_list.count += 1
 
     @staticmethod
+    def menu():
+        change = input("Что желаете: 1 - сдать в ремонт "
+                       "2 - посмотреть информацию ")
+        if change == "1": Cheke_list.new_client()
+        if change == "2":
+            search = input("Введите фамилию или номер квитанции ")
+            for i in range(0, len(b)):
+                if search == str(b[i][0]) or search in b[i][1]: print(b[i])
+
+
+
+
+
+    @staticmethod
     def new_client():
 
         name = input("Введите свое ФИО ")
@@ -37,7 +51,6 @@ class Cheke_list():
             chek3 = Monitor(Cheke_list.count, type, date, "18.05.2022", name, "в процессе", mark, size, description)
 
 
-
 class Phone(Cheke_list):
     def __init__(self, number, type, date, date_res, name, status, mark, o_s, description):
         super().__init__(number, type, date, date_res, name, status)
@@ -60,7 +73,6 @@ class Book(Cheke_list):
              self.status, self.mark, self.o_s, self.ear, self.description]
         b.append(a)
 
-
 class Monitor(Cheke_list):
     def __init__(self, number, type, date, date_res, name, status, mark, size, description):
         super().__init__(number, type, date, date_res, name, status)
@@ -70,7 +82,6 @@ class Monitor(Cheke_list):
         a = [self.number, self.name, self.type, self.date, self.date_res,
              self.status, self.mark, self.size, self.description]
         b.append(a)
-#    def print_chek(self):
 
 
 chek2 = Book(Cheke_list.count, "ноутбук", "11.05.2022", "16.05.2022", "Иван Иванович Макашов",
@@ -80,13 +91,13 @@ chek5 = Monitor(Cheke_list.count, "телевизор", datetime.date.today().st
 chek6 = Phone(Cheke_list.count, "телефон", datetime.date.today().strftime("%d.%m.%Y"), "21.05.2022", "Козловский Данила",
                 "принят в работу", "nokia", "android", "побит экран")
 
-Cheke_list.new_client()
+Cheke_list.menu()
 
 
 
 print("Всего квитанций", Cheke_list.count-1)
-for i in b:
-    print (i)
+#for i in b:
+#    print(i)
 
 
 
